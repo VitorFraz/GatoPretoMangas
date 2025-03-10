3118203b8a7af05ed3a7a1b698e161993eccebca
Fraz <vfbarduco@gmail.com>
2025-03-10 16:12:13 -0300

    correção de erros
    10/03/25

--- includes/cadastro_card.php
+++ includes/cadastro_card.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="PT-BR">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./assets/css/cadastro_card_style.css">
     <title>Gato Preto Mangás - Cadastro</title>
 </head>
 <body>
