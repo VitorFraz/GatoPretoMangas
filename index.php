bba882e330530fd773bb20674afada1bd4535134
Walmir Sousa <walmir.sousaa@gmail.com>
2025-04-04 11:59:49 -0300

    index

--- index.php
+++ index.php
@@ -4,13 +4,18 @@ require './classes/Mangas.php';
 
 $titulo = 'Gato Preto Mangas - Início';
 include './includes/header.php'; 
+// include './includes/banner.php';
 
  $mangas = new Mangas();
 $dados = $mangas->listarMangasBanco();
 
  echo $dados;
 
+<<<<<<< HEAD
  include './includes/manga_lista.php';
+=======
+include './includes/manga_lista.php';
+>>>>>>> c74024866e0e40589c1b291d4bfdae0eed9a1add
 
 include './includes/footer.php'; 
 ?>
