0d01d06910fef1f6b6126e5296861151fc9f0480
Walmir Sousa <walmir.sousaa@gmail.com>
2025-04-04 11:56:15 -0300

    atualização

--- includes/manga_card.php
+++ includes/manga_card.php
@@ -1,40 +1,23 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-<html lang="pt-br">
+<div class="col-lg-<?=isset($qntd) ? $qntd : 3 ?> col-md-6 col-sm-12">
+                    <figure>
+                        <!-- se começa primeiro a tag  figure para prossegir com o figcapture  -->
+                        <img src="./assets/img/poster/<?=$value['poster'] ?>"
+                            alt="Poster do filme Avatar o caminho da agua" class="foto-produto">
+                        <figcaption>
+                            <!-- conteudo do card colocar o que esta logo abaixo da foto; -->
+                            <h4><?php echo $value['nome'] ?></h4>
+                            <!-- o span é o elemento de value, adequado para aplicar estilos aocontrario da div que é um elemento em bloco  -->
+                            <span class="preco"><?php echo $value['valor_ingresso'] ?></span>
+                            <p class="descricao"><?php echo $value['descricao'] ?></p>
+                        </figcaption>
+                        <span class="genero">
+                            
+                            <?php foreach ($generosMangas as $value2) { ?>
+                            <label style="background-color: #<?= $value2['cor'] ?>;"> <?= $value2['nome'] ?> </label>
 
-<?php
+                            <?php } ?>
 
-// editar ainda o cards 
-
-$dsn = 'mysql:dbname=bd_gato_preto;host=127.0.0.1';
-$user = 'root';
-$password = '';
-
-$banco = new PDO($dsn, $user, $password);
-
-$select = 'SELECT * FROM tb_manga';
-
-$resultado = $banco->query($select)->fetchAll();
-
-//echo '<pre>'; // pre serve para organizar a tela
-//var_dump($resultado);
-?>
-<?php foreach ($resultado as $linha) { ?>
-    <div class="col col-3">
-        <figure>
-            <!-- se começa primeiro a tag  figure para prossegir com o figcapture  -->
-            <img src="./assets/img/manga<?php echo $linha['manga'] ?>"
-                alt="Imagem dos mangas" class="foto-manga">
-            <figcaption>
-                <!-- conteudo do card colocar o que esta logo abaixo da foto; -->
-                <h4><?php echo $linha['nome'] ?></h4>
-                <!-- o span é o elemento de linha, adequado para aplicar estilos aocontrario da div que é um elemento em bloco  -->
-                <span class="status"><?php echo $linha['status'] ?></span>
-                <p class="descricao"><?php echo $linha['descricao'] ?></p>
-            </figcaption>
-            <span class="genero">
-                <label style="<?php echo $linha['genero'] ?>"></label>
-            </span>
-        </figure>
-    </div>
-<?php } ?>
+                        </span>
+                    </figure>
+                </div>
